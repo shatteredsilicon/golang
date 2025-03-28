a5c1a95131e6346188e98a6a9caa9628a27bfd68
Thien Nguyen <nthien86@gmail.com>
2025-03-29 05:12:44 +0700

    Package go-1.20.12 for el7

--- rpmbuild/SOURCES/prep-golang.sh
+++ rpmbuild/SOURCES/prep-golang.sh
@@ -2,9 +2,11 @@
 
 set -e
 
-GO_VERSION="1.18.9"
+GO_VERSION="1.20.12"
 PKG_RELEASE="1"
 
 rm -f *.tar.gz
+
+wget https://github.com/golang/go/archive/refs/tags/go${GO_VERSION}.tar.gz
 
 wget https://github.com/golang-fips/go/archive/refs/tags/go${GO_VERSION}-${PKG_RELEASE}-openssl-fips.tar.gz
